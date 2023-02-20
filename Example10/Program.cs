68e7a27ebf88189fe147846c9c3efb63769700f0
Andrey_Kiselev <fant0m2311@gmail.com>
2023-02-21 02:02:01 +0300

    Ex10 decided

---
+++ Example10/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int secondOf3(int numeral)
+{
+   return (numeral % 100) / 10;
+}
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>99 && number<1000)
+{
+   int result = secondOf3(number);
+   Console.WriteLine(result);
+}
+else
+{
+   Console.WriteLine("Число не трёхзначное");
+}
+
+// с использованием Random:
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+Console.WriteLine(secondOf3(num));
