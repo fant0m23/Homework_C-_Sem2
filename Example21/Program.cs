db6e418a51cc8ecb63b315b50982e9e7f0a0cb93
Andrey_Kiselev <fant0m2311@gmail.com>
2023-02-21 10:56:44 +0300

    Ex21 decided

--- Example21/Program.cs
+++ Example21/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая проверяет пятизначное число на палиндром.
+
+void fillArray5digits(int[] array, int num)
+{
+   int index = 4;
+   while (index >= 0)
+   {
+      array[index] = num % 10;
+      num = num / 10;
+      index -= 1;
+   }
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int number = new Random().Next(10000, 100000);
+// Console.WriteLine(number);
+int[] mass = new int[5];
+fillArray5digits(mass, number);
+
+if (mass[0]==mass[4] && mass[1]==mass[3])
+{
+   Console.WriteLine("Это палиндром");
+}
+else
+{
+   Console.WriteLine("Это HE палиндром");
+}
+
