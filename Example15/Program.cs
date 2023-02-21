92ff253bf2d8ecdb022d48473789ebbc07a349b5
Andrey_Kiselev <fant0m2311@gmail.com>
2023-02-21 10:40:00 +0300

    Ex15 decided

---
+++ Example15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool holiday(int numberOfDay)
+{
+   return numberOfDay < 6;
+}
+
+int day = new Random().Next(1, 8);
+Console.WriteLine(day);
+bool result = holiday(day);
+Console.WriteLine(result);
