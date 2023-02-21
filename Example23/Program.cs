8c23891b9aa21106269c8a44fa5cc30ee9007015
Andrey_Kiselev <fant0m2311@gmail.com>
2023-02-21 14:29:38 +0300

    Ex23 decided

--- Example23/Program.cs
+++ Example23/Program.cs
@@ -3,5 +3,43 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+void fillArrayQrt3(double[] array, int size)
+{
+   int 
+      index = 0;
+   while (index < size)
+   {
+      array[index] = Math.Pow((index+1), 3);
+      index++;
+   }
+}
+// void printTable(double[] array)
+// {
+//    int size = array.Length,
+//       index = 0;
+//    while (index < size)
+//    {
+//       Console.WriteLine($"{(index + 1)} x {(index + 1)} x {(index + 1)} = {array[index]}");
+//       index++;
+//    }
+//    Console.WriteLine();
+// }
+void printArray(double[] array)
+{
+   int size = array.Length,
+      index = 0;
+   while (index < size)
+   {
+      Console.Write(array[index]);
+      Console.Write("  ");
+      index++;
+   }
+   Console.WriteLine();
+}
 
-Console.WriteLine();
+Console.Write("Введите число: ");
+int nums = Convert.ToInt32(Console.ReadLine());
+double[] mass = new double[nums];
+fillArrayQrt3(mass, nums);
+printArray(mass);
+// printTable(mass);
