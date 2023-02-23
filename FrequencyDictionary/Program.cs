c3975b39c45ad44adda7cc4c258001beac5dec52
Andrey_Kiselev <fant0m2311@gmail.com>
2023-02-24 00:30:34 +0300

    additional tasks decided

--- FrequencyDictionary/Program.cs
+++ FrequencyDictionary/Program.cs
@@ -9,4 +9,50 @@
 // 4 - 2р
 // 5 - 2р
 
+int Request(string message)
+{
+   Console.WriteLine(message);
+   int value = Convert.ToInt32(Console.ReadLine());
+   return value;
+}
+void FillArray(int[] array, int end)
+{
+   int size = array.Length;
+   for (int index = 0; index < size; index++)
+   {
+      array[index] = new Random().Next(1, end + 1);
+   }
+}
+void PrintArray(int[] array)
+{
+   int size = array.Length;
+   for (int index = 0; index < size; index++)
+   {
+      Console.Write(array[index] + "  ");
+   }
+   Console.WriteLine();
+}
+int FreqCount(int[] array, int find)
+{
+   int count = 0,
+      size = array.Length;
+   for (int index = 0; index < size; index++)
+   {
+      if (array[index] == find) count++;
+   }
+   return count;
+}
 
+string msg1 = "Введите размер массива: ";
+int length = Request(msg1);
+int[] mass = new int[length];
+string msg2 = "Массив наполнен числами от 1 до  ";
+int lot = Request(msg2);
+
+FillArray(mass, lot);
+PrintArray(mass);
+
+for (int numbers = 1; numbers <= lot; numbers++)
+{
+   Console.WriteLine(numbers + " - " + FreqCount(mass, numbers) + " p");
+}
--- MixArray/Program.cs
+++ MixArray/Program.cs
@@ -1,12 +1,50 @@
 ﻿// Реализовать перемешивание массива
 
-// дан массив: наполненый числами от 1 до N
-// Задача: сколько раз какое число встречается (построить частотный словарь)
+int Request(string message)
+{
+   Console.WriteLine(message);
+   int value = Convert.ToInt32(Console.ReadLine());
+   return value;
+}
+void FillArray(int[] array)
+{
+   int size = array.Length;
+   for (int index = 0; index < size; index++)
+   {
+      array[index] = new Random().Next(0, 10);
+   }     
+}
+void PrintArray(int[] array)
+{
+   int size = array.Length;
+   for (int index = 0; index < size; index++)
+   {
+      Console.Write(array[index] + "  ");
+   }
+   Console.WriteLine();
+}
+void MixArray(int[] array, int mixing)
+{
+   int temp = 0, indexA = 0, indexB = 0,
+   size = array.Length;
+   for (int count = 0; count < mixing; count++)
+   {
+      indexA = new Random().Next(0, size);
+      temp = array[indexA];
+      indexB = new Random().Next(0, size);
+      array[indexA] = array[indexB];
+      array[indexB] = temp;
+      count++;
+   }
+}
 
-// 1 3 1 2 1 2 4 5 1 2 5 4
+string msg1 = "Введите размер массива: ";
+int length = Request(msg1);
+int[] mass = new int[length];
+string msg2 = "Введите количество перестановок элементов массива: ";
+int lotsMixing = Request(msg2);
 
-// 1 - 4р
-// 2 - 3р
-// 3 - 1р
-// 4 - 2р
-// 5 - 2р
+FillArray(mass);
+PrintArray(mass);
+MixArray(mass, lotsMixing);
+PrintArray(mass);
