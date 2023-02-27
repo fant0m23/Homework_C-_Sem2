87858c1885ccd784ad2ab1662ac81632faf84dea
Andrey_Kiselev <fant0m2311@gmail.com>
2023-02-27 18:49:29 +0300

    Ex25 decided

---
+++ Example25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// нельзя использовать Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetValueUser(string message)
+{
+   Console.WriteLine(message);
+   int value = Convert.ToInt32(Console.ReadLine());
+   return value;
+}
+int MyMathPow(int number, int exp)
+{
+   int result = 1;
+   for (int i = 0; i < exp; i++)
+   {
+      result = number * result;
+   }
+   return result;
+}
+
+string requestA = "Введите число A: ";
+int numberA = GetValueUser(requestA);
+string requestB = "Введите число B: ";
+int numberB = GetValueUser(requestB);
+
+int result = MyMathPow(numberA, numberB);
+Console.WriteLine(result);
